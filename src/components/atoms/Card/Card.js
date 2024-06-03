0b429efad89b6f7113712d246a8b8a1ae5e51913
Guilherme Eger <guilhermeeger.developer@gmail.com>
2024-06-04 08:45:23 +1000

    Folder structure builded

---
+++ src/components/atoms/Card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props){
+  return (
+    <div>
+        <div className="cardField" />
+    </div>
+  )
+};
+
+export default Card;
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import GameField from "../../organisms/GameField/GameField";
+
+function Home(props){
+  return (
+    <div>
+      <GameField />
+    </div>
+  )
+};
+
+export default Home;
